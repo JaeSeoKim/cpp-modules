361523995349a517b8b500118ecaa98e99f496bf
JaeSeoKim <devjaeseo@gmail.com>
2021-07-14 18:09:39 +0900

    :bug: fix: Change some code (m03-ex03)

--- module-03/ex03/src/DiamondTrap.cpp
+++ module-03/ex03/src/DiamondTrap.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap() : ClapTrap(), ScavTrap(), FragTrap()
+DiamondTrap::DiamondTrap() : ClapTrap("default_clap_name"), ScavTrap(), FragTrap()
 {
 	std::cout << "🤖 FragTrap(void)" << std::endl;
+	this->name = "default";
+	setHitPoints(100);
+	setEnergyPoints(50);
+	setAttackDamage(30);
 }
-DiamondTrap::DiamondTrap(std::string const &name) : ClapTrap(name), ScavTrap(name), FragTrap(name)
+DiamondTrap::DiamondTrap(std::string const &name) : ClapTrap(name + "_clap_name"), ScavTrap(), FragTrap()
 {
 	std::cout << "🤖 FragTrap(" << name << ")" << std::endl;
 	this->name = name;
